ed4c0213fa6b5f11744282f07a101438d309ae2e
Juan Ponce <juan.ponce@vera.com.uy>
2017-09-24 00:50:43 -0300

    Add files via upload
    Responsive logrado. 24/9/17 a las 00:50

--- tools.js
+++ tools.js
@@ -28,3 +28,25 @@ var x = setInterval(function() {
         document.getElementById("CountDown").innerHTML = "Ofertas del dia - EXPIRED";
     }
 }, 1000);
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn') && !event.target.mathes('#busca')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
