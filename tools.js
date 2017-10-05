861c4839a50dad21fd7fdce299244876079f5d7f
Juan Ponce <juan.ponce@vera.com.uy>
2017-10-04 22:54:09 -0300

    Oferta
    Con errores

--- tools.js
+++ tools.js
@@ -1,3 +1,5 @@
+
+function Reloj(){
 // Set the date we're counting down to
 var  tomorrow = new Date();
 tomorrow.setDate(tomorrow.getDate()+1);
@@ -28,7 +30,7 @@ var x = setInterval(function() {
         document.getElementById("CountDown").innerHTML = "-- EXPIRED --";
     }
 }, 1000);
-
+}
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -49,4 +51,47 @@ window.onclick = function(event) {
       }
     }
   }
+} 
+
+ if (document.addEventListener){
+        window.addEventListener('load',banner(),false);
+    } else {
+        window.attachEvent('onload',banner());
+    }
+
+function banner(){
+    switch( Math.floor((Math.random() * 10) + 1)) {
+    case 1:
+        document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/Banner900200.jpg')"; 
+        break;
+    case 2
+         document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/BannerDollar.jpg')"; 
+        break:
+    case 3
+         document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/BannerMoney.jpg')"; 
+        break;
+    case 4
+        document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/BannerOrange.jpg')"; 
+        break;
+    case 5
+        document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/CityBanner.jpg')"; 
+        break;
+    case 6
+        document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/illusion.jpg')"; 
+        break;
+    case 7
+        document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/OfertaBanner.jpg')"; 
+        break;
+    case 8
+        document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/passage.jpg')"; 
+        break;
+    case 9
+        document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/ArtBanner.jpg')"; 
+        break;
+    case 10
+        document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/TechnoBanner.jpg')"; 
+        break;   
+    default:
+        document.getElementById("Banner").style.backgroundImage = "url('images/RandomBanner/ArtBanner.jpg')"; 
+        break;
 } 
