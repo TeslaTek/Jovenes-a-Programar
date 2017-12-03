018cdc9121ca39cf5633da164eda2f0bab1fcb9d
Juan Ponce <juan.ponce@vera.com.uy>
2017-12-03 02:21:38 -0300

    carrito inmundo
    basta

--- resources/js/productos.js
+++ resources/js/productos.js
@@ -1,3 +1,6 @@
+var baseUrl = "http://localhost:3000/";
+
+
 // Mediante una llamada ajax, consume el API rest
 // Obtiene el listado de productos
 // Despliega los datos
@@ -53,6 +56,43 @@ $( "#logoIndex" ).click(function() {
     mostrarProductos ();
 });
 
+$( "#carrito" ).click(function() {
+        $("#popup").addClass("show");
+
+
+        $.ajax({
+        url: "http://localhost:3000/productos",
+        contentType: "application/json",
+        type: "GET"
+    }).done(function(data) {  
+        // Tengo los datos 
+        $.each(data, function(i, item) {
+            
+            if ( data[i].compra == 1){
+           var carrito = $("#carroIN");
+        // Generamos el nodo html con los datos que vienen en el JSON 
+        var html = '<div  class="carro" id="'+ data[i].id +'" style="background-image: url('+data[i].imagen+')"  onclick="quitar(this.id)">' + 
+        '<div class="dropbtn btnCarro" >Eliminar</div></div>' ; 
+        // Agregamos en el contenedor de  carrito el html para cada dato del listado
+        carrito.append(html);
+        }
+        })
+        var botones = '<div> <button onclick="vaciar()" class="dropbtn">Vaciar Carro</button> <button onclick="pagar()" class="dropbtn">Pagar</button></div>'
+        $("#carroIN").append(botones);
+
+    });
+
+
+});
+
+function cerrar(){
+
+    $("#popup").removeClass("show");
+      $("#popup").empty();
+      var contenido = '<div id="carroIN" class="carroInterno"> <h2>CARRITO</h2> <button onclick="cerrar()" class="cerrarBoton">Cerrar</button></div>  </div>';
+      $("#popup").append(contenido);
+}
+
 
 function ofertum(id) {
     $("#products").empty();
@@ -73,8 +113,8 @@ function cambioPagina(data,x) {
         var products = $("#products");
         // Generamos el nodo html con los datos que vienen en el JSON 
         if ( data[i].id == x ){
-        var html = '<img class="imgReview" id="'+ data[i].id +'" src='+data[i].imagenReview+' >' + 
-        '<div class="Titulo">'+ data[i].titulo + '</div>' + '<div class="review">'+data[i].review+'</div>' ; 
+        var html = '<img class="imgReview"  src='+data[i].imagenReview+' >' + 
+        '<div class="Titulo">'+ data[i].titulo + '</div>' + '<div class="review">'+data[i].review+'</div>' + ' <button id="'+ data[i].id +'" onclick="cargarCompra(this.id)" class="dropbtn">Comprar</button>' ; 
         // Agregamos en el contenedor de productos el html para cada dato del listado
          products.append(html);}
         });
@@ -88,7 +128,7 @@ function categoria(factor){
 
 
 $.ajax({
-        url: "http://localhost:3000/productos",
+        url: "http://localhost:3000/productos" ,
         contentType: "application/json",
         type: "GET"
     }).done(function(data) {  
@@ -128,7 +168,7 @@ $.ajax({
         $.each(data, function(i, item) {
 
             
-            if ( data[i].review.indexOf(palabra) != -1){
+            if ( data[i].review.toLowerCase().indexOf(palabra) != -1){
                 bandera = true;
            var products = $("#products");
         // Generamos el nodo html con los datos que vienen en el JSON 
@@ -153,6 +193,132 @@ $.ajax({
 
 
 
+function cargarCompra(id){
+
+   
+  var resourceUrl = "productos"; 
+
+$.ajax({
+        url: returnRequestUrl(resourceUrl + "/" + id),
+        contentType: "application/json",
+        type: "GET"
+    }).done(function(data) {  
+        
+        data.compra=1;
+ 
+$.ajax({
+        url: returnRequestUrl(resourceUrl + "/" + id),
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        type: 'PUT'
+    }).done(function(data) {
+     
+    });
+
+})
+
+}
+
+
+function quitar(id){
+
+
+
+  var resourceUrl = "productos"; 
+
+$.ajax({
+        url: returnRequestUrl(resourceUrl + "/" + id),
+        contentType: "application/json",
+        type: "GET"
+    }).done(function(data) {  
+  
+        data.compra=0;
+ 
+$.ajax({
+        url: returnRequestUrl(resourceUrl + "/" + id),
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        type: 'PUT'
+    }).done(function(data) {
+        
+        $("#popup").empty();
+      var contenido = '<div id="carroIN" class="carroInterno"> <h2>CARRITO</h2> <button onclick="cerrar()" class="cerrarBoton">Cerrar</button></div>  </div>';
+      $("#popup").append(contenido);
+
+$.ajax({
+        url: "http://localhost:3000/productos",
+        contentType: "application/json",
+        type: "GET"
+    }).done(function(data) {  
+        // Tengo los datos 
+        $.each(data, function(i, item) {
+            
+            if ( data[i].compra == 1){
+           var carrito = $("#carroIN");
+        // Generamos el nodo html con los datos que vienen en el JSON 
+        var html = '<div  class="carro" id="'+ data[i].id +'" style="background-image: url('+data[i].imagen+')"  onclick="quitar(this.id)">' + 
+        '<div class="dropbtn btnCarro" >Eliminar</div></div>' ; 
+        // Agregamos en el contenedor de  carrito el html para cada dato del listado
+        carrito.append(html);
+        }
+        })
+        var botones = '<div> <button onclick="vaciar()" class="dropbtn">Vaciar Carro</button> <button onclick="pagar()" class="dropbtn">Pagar</button></div>'
+        $("#carroIN").append(botones);
+
+    });
+    });
+
+});
+
+
+}
+
+
+function vaciar(){
+var resourceUrl = "productos";
+
+$.ajax({
+        url: "http://localhost:3000/productos" ,
+        contentType: "application/json",
+        type: "GET"
+    }).done(function(data) {  
+   
+        // Tengo los datos 
+        $.each(data, function(i, item) {
+            
+            if ( data[i].compra == 1){
+        
+
+          data[i].compra =0;
+          var id = i+1;      
+       $.ajax({
+        url: returnRequestUrl(resourceUrl + "/" + id),
+        contentType: "application/json",
+        data: JSON.stringify(data[i]),
+        type: 'PUT'
+    }).done(function(data) {
+    
+        $("#popup").empty();
+      var contenido = '<div id="carroIN" class="carroInterno"> <h2>CARRITO</h2> <button onclick="cerrar()" class="cerrarBoton">Cerrar</button></div>  </div>';
+      $("#popup").append(contenido);
+     
+         });
+    }
+    })
+    });
+
+
+
+}
+
+function pagar(){
+
+   window.location.replace("https://www.paypal.com/uy/");
+}
+
+function returnRequestUrl(resourceUrl) {
+    return baseUrl + resourceUrl;
+}
 
 
 
